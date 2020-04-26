6794aa180d815a0d7cab72c25a48f9e35d43e21b
denny.k <denny.k@kakaocorp.com>
2020-04-26 13:52:31 +0900

    Block Basic

--- proj_01/proj_01/main.m
+++ proj_01/proj_01/main.m
@@ -7,41 +7,22 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "Fraction.h"
 
-#define MakeFract(x, y) ([[Fraction alloc] initWith: x over: y])
-
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        Fraction *a = [[Fraction alloc] init];
-        Fraction *b = [[Fraction alloc] init];
-        Fraction *c = MakeFract(3, 8);
-        Fraction *result;
+void (^calculateNumber) (int) = {
+    ^(int n) {
+        int i, resultNumber = 0;
         
-        [a setTo:1 over:3];
-        [b setTo:2 over:5];
-        
-        [c print];
-        
-        [a print]; NSLog(@"  +"); [b print]; NSLog(@"----");
-        result = [a add: b];
-        [result print];
-        NSLog(@"\n");
-        
-        [a print]; NSLog(@"  +"); [b print]; NSLog(@"----");
-        result = [a sub: b];
-        [result print];
-        NSLog(@"\n");
-        
-        [a print]; NSLog(@"  +"); [b print]; NSLog(@"----");
-        result = [a mul: b];
-        [result print];
-        NSLog(@"\n");
-        
-        [a print]; NSLog(@"  +"); [b print]; NSLog(@"----");
-        result = [a div: b];
-        [result print];
-        NSLog(@"\n");
+        for (i = 1; i <= n; ++i) {
+            resultNumber += i;
+        }
+        NSLog(@"Result Number from 1 to %i is %i", n, resultNumber);
     }
-    return 0;
+};
+
+int main(int argc, char *argv[]) {
+    @autoreleasepool {
+        calculateNumber(10);
+        calculateNumber(50);
+        calculateNumber(100);
+    }
 }
